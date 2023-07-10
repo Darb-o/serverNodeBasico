f4efb432c9f290cb32f80615973344a0d6393fea
Darb-o <83312668+Darb-o@users.noreply.github.com>
2023-07-09 19:06:13 -0500

    se añade un puerto por defecto si es locamente

--- models/server.js
+++ models/server.js
@@ -8,7 +8,7 @@ class Server {
         //Invoca a express
         this.app = express();
         //El puerto queda como variable de entorno
-        this.port = process.env.PORT;
+        this.port = process.env.PORT || 3000;
         this.usuariosPath = '/api/usuarios';
         //Middlewares
         this.middlewares();
